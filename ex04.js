51793364f1496ec9df874c8202e59f9c6b06d2a6
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-08 14:08:02 +0400

    Avoid Mutations and Side Effects Using Functional Programming

---
+++ ex04.js
@@ -0,0 +1,11 @@
+// 4. Заполните код функции incrementer так, чтобы она возвращала значение глобальной переменной, fixedValue в кубе.
+
+
+
+let fixedValue = 4;
+
+function incrementer () {
+    return fixedValue**3;
+}
+
+console.log(incrementer());
